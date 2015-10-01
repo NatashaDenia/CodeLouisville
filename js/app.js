131b03869fc7bc38432db264b8ba4e686e25161a
NatashaDenia <natashadenia@gmail.com>
2015-10-01 19:19:41 -0400

    Added Reveal Answer on FAQ

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Prevent answer from being seen
+//Prevent answer from being seen/*From api.jquery.com*/
 //Solution: Hide answer and reveal them through user interaction
 
 //1, Hide answer
@@ -6,5 +6,9 @@ $(".answer span").hide();
 //2, Add a button
 $(".answer").append("<button>Reveal Answer.</button>");
 // 3, When button pressed
+$("button").click(function(){ /*Events*/
   //3.1, Show answer
+  $(".answer span").show();
   //3.2, Get rid of button
+  $(this).remove();  /*Manipulation*/
+});
